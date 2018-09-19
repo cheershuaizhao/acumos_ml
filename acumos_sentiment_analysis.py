206de7dca7ebad82951b091fa454501eda52bd26
Shuai Zhao <shuaizhao92@gmail.com>
2018-09-19 12:26:41 -0400

    Update acumos_sentiment_analysis.py

--- acumos_sentiment_analysis.py
+++ acumos_sentiment_analysis.py
@@ -1,19 +1,26 @@
 import numpy as np
-import seaborn as sn
 import pandas as pd
+
 from keras.datasets import imdb
-import matplotlib.pyplot as plt
 from keras.preprocessing import sequence
 from keras.utils import plot_model
-
-
 from keras.utils.data_utils import get_file
 from keras.datasets import imdb
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
-import pickle
-import numpy as np
+from keras.models import Model as KerasModel
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, concatenate, add, RepeatVector, Permute
+from keras.layers import Flatten, Merge, multiply, Lambda
+from keras.layers import Bidirectional, GlobalMaxPool1D, Conv1D, MaxPooling1D, AveragePooling1D, GlobalMaxPooling1D, GlobalAveragePooling1D
+from keras.callbacks import EarlyStopping
+from keras import backend as K
+
+from acumos.session import AcumosSession
+from acumos.auth import _authenticate, _configuration
+from acumos.modeling import Model
+from acumos.modeling import  create_namedtuple, List
+
 
 embed_size = 50 # max embedding length for a word
 max_length = 200 # max number of words in a review
@@ -45,6 +52,7 @@ x_train = t.texts_to_sequences(x_train)
 padded_docs = pad_sequences(x_train, maxlen=max_length, padding='post')
 
 #use pre-trained Google word embedding
+#to download at here https://code.google.com/archive/p/word2vec/
 embeddings_index = dict()
 f = open('/home/shuai/repo/toxic comment classfication/glove.6B/glove.6B.50d.txt')
 for line in f:
@@ -63,14 +71,6 @@ for word, i in t.word_index.items():
 
 
 #build the model
-from keras.models import Model as KerasModel
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, concatenate, add, RepeatVector, Permute
-from keras.layers import Flatten, Merge, multiply, Lambda
-from keras.layers import Bidirectional, GlobalMaxPool1D, Conv1D, MaxPooling1D, AveragePooling1D, GlobalMaxPooling1D, GlobalAveragePooling1D
-from keras.callbacks import EarlyStopping
-from keras import backend as K
-
-
 #RCNN model
 inp1 = Input(shape=(max_length,))
 x = Embedding(vocab_size, embed_size, weights=[embedding_matrix])(inp1)
@@ -95,8 +95,20 @@ rcnn_model.fit(padded_docs, y_train, batch_size=128, epochs=10, verbose=1, callb
       
 #rcnn_model.save('rcnn_model.h5')
 
-from acumos.modeling import  create_namedtuple, List
-
-Input = create_namedtuple('Input', [('review', List[str]), ('g_truth', List[float])])
+Input = create_namedtuple('Input', [('review', List[str])])
 Output = create_namedtuple('Output', [('sentiment', List[float])])
+
+
+def predict(X: Input)-> Output:
+    x_test = t.texts_to_sequences(list(X.review))
+    padded_docs_test = pad_sequences(x_test, maxlen=max_length, padding='post')
+    yhat = rcnn_model.predict(padded_docs_test, batch_size=1024, verbose=1)
+    preds = Output(sentiment = [x[0] for x in yhat.tolist()])
+    return preds
+
+
+session = AcumosSession(push_api="https://acumos.research.att.com:8090/onboarding-app/v2/models", auth_api="https://acumos.research.att.com:8090/onboarding-app/v2/auth")
+acumos_model = Model(classify=predict)
+session.dump(acumos_model,'sentiment-analysis','./sentiment_analysis_dump/')
+#at last, upload the dumped files to Acumos via web onboarding
 
